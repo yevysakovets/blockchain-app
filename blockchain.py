dbe4b75ed1690d409a5dad4950a90c74271ff53c
yevysakovets <zhenyasakovets@gmail.com>
2024-08-22 00:04:42 -0500

    refactor some logic

--- blockchain.py
+++ blockchain.py
@@ -40,16 +40,18 @@ def print_blockchain_elements():
     for block in blockchain:
         print('Outputting Block')
         print(block)
+    print("-" * 20)
+    print("-" * 20)
 
 
 def verify_chain():
     block_index = 0
     is_valid = True
-    for block in blockchain:
+    for block_index in range(len(blockchain)):
         if block_index == 0:
             block_index += 1
             continue
-        elif block[0] == blockchain[block_index - 1]:
+        elif blockchain[block_index] == blockchain[block_index - 1]:
             is_valid = True
         else:
             is_valid = False
@@ -58,7 +60,9 @@ def verify_chain():
     return is_valid
 
 
-while True:
+waiting_for_input = True
+
+while waiting_for_input:
     print('Please choose')
     print('1: Add a new transaction value')
     print('2: Output the blockchain blocks')
@@ -74,10 +78,11 @@ while True:
         if len(blockchain) >= 1:
             blockchain[0] = [2]
     elif user_choice == 'q':
-        break
+        waiting_for_input = False
     else:
         print('Input was invalid, please pick a value from the list!')
     if not verify_chain():
+        print_blockchain_elements()
         print('Invalid blockchain!')
         break
 
